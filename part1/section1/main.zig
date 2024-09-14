125e5169430d0360149e7c448b49ae0ceb5ba867
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-14 21:20:58 +0100

    think that's reading a printing bits properly

--- part1/section1/main.zig
+++ part1/section1/main.zig
@@ -4,6 +4,12 @@
 
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("It works\n", .{});
+pub fn main() !void {
+    const file = try std.fs.openFileAbsolute("/home/matt/code/perf-aware/part1/section1/single_register_mov.asm", .{});
+    const file_reader = file.reader();
+
+    const first_byte = try file_reader.readByte();
+    const second_byte = try file_reader.readByte();
+
+    std.debug.print("{b} {b}\n", .{ first_byte, second_byte });
 }
