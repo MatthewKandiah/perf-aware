0bdaea8bead75d9ef072b09551203ca6cfc9b19a
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-14 14:19:50 +0100

    zig build boilerplate

---
+++ part1/section1/main.zig
@@ -0,0 +1,9 @@
+// Task:
+// input - file containing compiled binary instructions
+// output - valid assembly that compiles to matching binary instructions
+
+const std = @import("std");
+
+pub fn main() void {
+    std.debug.print("It works\n", .{});
+}
