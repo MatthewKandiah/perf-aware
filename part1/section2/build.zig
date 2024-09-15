3bb23e2c16cffedf941aa6d9f4ba92ce384e9e40
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-15 21:18:38 +0100

    copied inputs and setup zig boilerplate

--- part1/section2/build.zig
+++ part1/section2/build.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "main",
-        .root_source_file = b.path("main.zig"),
         .target = b.host,
+        .root_source_file = .{ .cwd_relative = "main.zig" },
     });
 
     b.installArtifact(exe);
---
+++ part1/section2/main.zig
@@ -0,0 +1,7 @@
+// input: binary instruction file containing different variants of mov instruction
+// output: decoded instructions, which can be assembled into binary instructions exactly matching input
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("It works\n", .{});
+}
