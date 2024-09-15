0e052c22ce849b38603c581684234f1017a261db
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-15 08:05:19 +0100

    read instruction by instruction from multi-instruction file

--- part1/section1/main.zig
+++ part1/section1/main.zig
@@ -5,12 +5,14 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const file = try std.fs.openFileAbsolute("/home/matt/code/perf-aware/part1/section1/single_register_mov", .{});
+    // const file = try std.fs.openFileAbsolute("/home/matt/code/perf-aware/part1/section1/single_register_mov", .{});
+    const file = try std.fs.openFileAbsolute("/home/matt/code/perf-aware/part1/section1/many_register_mov", .{});
     const file_reader = file.reader();
 
     while (true) {
-        const read_byte_result = file_reader.readByte() catch break;
-        std.debug.print("{b}\n", .{read_byte_result});
+        const byte1 = file_reader.readByte() catch break;
+        const byte2 = try file_reader.readByte();
+        std.debug.print("{b} {b}\n", .{ byte1, byte2 });
     }
 
     std.debug.print("Done\n", .{});
