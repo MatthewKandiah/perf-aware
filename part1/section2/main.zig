f39f511cdcbb5cc8651352c620768d997aed3459
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-18 15:25:21 +0100

    wip part 2

--- part1/section2/main.zig
+++ part1/section2/main.zig
@@ -12,43 +12,74 @@ pub fn main() !void {
     const output_file = try std.fs.createFileAbsolute("/home/matt/code/perf-aware/part1/section2/more_movs_out.asm", .{});
     defer output_file.close();
 
-    const op_code_mask = 0b11111100;
-    const d_mask = 0b00000010;
-    const w_mask = 0b00000001;
-    const mod_mask = 0b11000000;
-    const reg_mask = 0b00111000;
-    const rm_mask = 0b00000111;
-
     _ = try output_file.write("bits 16\n\n");
 
     while (true) {
-        const byte1 = file_reader.readByte() catch break;
-        const op_code = (byte1 & op_code_mask) >> 2;
-        const d = (byte1 & d_mask) >> 1;
-        const w = byte1 & w_mask;
+        const first_byte = file_reader.readByte() catch break;
 
-        if (op_code != 0b100010) std.debug.panic("Unsupported op_code {b}", .{op_code});
+        if ((first_byte & imm_to_reg_opcode_mask) >> 4 == imm_to_reg_opcode) {
+            std.debug.print("imm_to_reg operation\n", .{});
+        } else if ((first_byte & mov_opcode_mask) >> 2 == mov_opcode) {
+            const d = (first_byte & mov_d_mask) >> 1;
+            const w = (first_byte & mov_w_mask);
+            const second_byte = try file_reader.readByte();
+            const mod = (second_byte & mov_mod_mask) >> 6;
+            const reg = (second_byte & mov_reg_mask) >> 3;
+            const r_m = (second_byte & mov_r_m_mask);
 
-        const byte2 = try file_reader.readByte();
-        const mod = (byte2 & mod_mask) >> 6;
-        const reg = (byte2 & reg_mask) >> 3;
-        const rm = byte2 & rm_mask;
+            switch (mod) {
+                0b00 => {
+                    const addr_byte1 = if (r_m == 0b110) try file_reader.readByte() else null;
+                    const addr_byte2 = if (r_m == 0b110) try file_reader.readByte() else null;
+                    const direct_address: u16 = if (addr_byte1 != null and addr_byte2 != null) (@as(u16, @intCast(addr_byte2.?)) << 8) & (@as(u16, @intCast(addr_byte2.?))) else undefined;
+                    _ = try output_file.write("mov ");
+                    _ = try output_file.write(try r_m_mod0_lookup(r_m, direct_address));
+                    _ = try output_file.write("\n");
+                },
+                0b01 => {
+                    const displacement = try file_reader.readByte();
+                    _ = try output_file.write("mov ");
+                    _ = try output_file.write(try r_m_mod1_lookup(r_m, displacement));
+                    _ = try output_file.write("\n");
+                },
+                0b10 => {
+                    const disp_lo = try file_reader.readByte();
+                    const disp_hi = try file_reader.readByte();
+                    const displacement: u16 = (@as(u16, @intCast(disp_hi)) << 8) | @as(u16, @intCast(disp_lo));
+                    _ = try output_file.write("mov ");
+                    _ = try output_file.write(try r_m_mod2_lookup(r_m, displacement));
+                    _ = try output_file.write("\n");
+                },
+                0b11 => {
+                    const reg_register = try reg_lookup(reg, w);
+                    const r_m_register = try reg_lookup(r_m, w);
+                    const source_register = if (d == 0) reg_register else r_m_register;
+                    const dest_register = if (d == 0) r_m_register else reg_register;
 
-        if (mod != 0b11) std.debug.panic("Unsupported mod {b}", .{mod});
-
-        const reg1 = try reg_lookup(reg, w);
-        const reg2 = try reg_lookup(rm, w);
-
-        const source = if (d == 0) reg1 else reg2;
-        const dest = if (d == 0) reg2 else reg1;
-
-        _ = try output_file.write("mov ");
-        _ = try output_file.write(dest.toString());
-        _ = try output_file.write(", ");
-        _ = try output_file.write(source.toString());
-        _ = try output_file.write("\n");
+                    _ = try output_file.write("mov ");
+                    _ = try output_file.write(dest_register.toString());
+                    _ = try output_file.write(", ");
+                    _ = try output_file.write(source_register.toString());
+                    _ = try output_file.write("\n");
+                },
+                else => unreachable,
+            }
+        }
     }
 }
+
+const mov_opcode = 0b100010;
+const mov_opcode_mask = 0b11111100;
+const mov_d_mask = 0b00000010;
+const mov_w_mask = 0b00000001;
+const mov_mod_mask = 0b11000000;
+const mov_reg_mask = 0b00111000;
+const mov_r_m_mask = 0b00000111;
+
+const imm_to_reg_opcode = 0b1011;
+const imm_to_reg_opcode_mask = 0b11110000;
+const imm_to_reg_w_mask = 0b00001000;
+const imm_to_reg_reg_mask = 0b00000111;
 
 const Register = enum {
     AX,
@@ -70,7 +101,7 @@ const Register = enum {
 
     const Self = @This();
 
-    fn toString(self: Self) []const u8 {
+    inline fn toString(self: Self) []const u8 {
         return switch (self) {
             .AX => "ax",
             .AL => "al",
@@ -91,6 +122,53 @@ const Register = enum {
         };
     }
 };
+
+fn r_m_mod0_lookup(r_m: u8, direct_address: ?u16) ![]const u8 {
+    return switch (r_m) {
+        0b000 => "[" ++ Register.BX.toString() ++ " + " ++ Register.SI.toString() ++ "]",
+        0b001 => "[" ++ Register.BX.toString() ++ " + " ++ Register.DI.toString() ++ "]",
+        0b010 => "[" ++ Register.BP.toString() ++ " + " ++ Register.SI.toString() ++ "]",
+        0b011 => "[" ++ Register.BP.toString() ++ " + " ++ Register.DI.toString() ++ "]",
+        0b100 => "[" ++ Register.SI.toString() ++ "]",
+        0b101 => "[" ++ Register.DI.toString() ++ "]",
+        0b110 => {
+            var buf: [64]u8 = undefined;
+            return std.fmt.bufPrint(&buf, "[{}]", .{direct_address.?});
+        },
+        0b111 => "[" ++ Register.BX.toString() ++ "]",
+        else => unreachable,
+    };
+}
+
+fn r_m_mod1_lookup(r_m: u8, displacement: u8) ![]const u8 {
+    var buf: [64]u8 = undefined;
+    return switch (r_m) {
+        0b000 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.SI.toString(), displacement }),
+        0b001 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.DI.toString(), displacement }),
+        0b010 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.SI.toString(), displacement }),
+        0b011 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.DI.toString(), displacement }),
+        0b100 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.SI.toString(), displacement }),
+        0b101 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.DI.toString(), displacement }),
+        0b110 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BP.toString(), displacement }),
+        0b111 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BX.toString(), displacement }),
+        else => unreachable,
+    };
+}
+
+fn r_m_mod2_lookup(r_m: u8, displacement: u16) ![]const u8 {
+    var buf: [64]u8 = undefined;
+    return switch (r_m) {
+        0b000 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.SI.toString(), displacement }),
+        0b001 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.DI.toString(), displacement }),
+        0b010 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.SI.toString(), displacement }),
+        0b011 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.DI.toString(), displacement }),
+        0b100 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.SI.toString(), displacement }),
+        0b101 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.DI.toString(), displacement }),
+        0b110 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BP.toString(), displacement }),
+        0b111 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BX.toString(), displacement }),
+        else => unreachable,
+    };
+}
 
 fn reg_lookup(reg: u8, w: u8) !Register {
     return switch (w) {
--- part1/section2/more_movs_out.asm
+++ part1/section2/more_movs_out.asm
@@ -2,3 +2,11 @@ bits 16
 
 mov si, bx
 mov dh, al
+mov [bx + si]
+mov [bp + di]
+mov [bp + 0]
+mov [bx + si + 4]
+mov [bx + si + 4999]
+mov [bx + di]
+mov [bp + si]
+mov [bp + 0]
