4b8ea94339ed52ccbc7e882e4312f6d17d935f5d
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-20 17:16:00 +0100

    using the helper functions in more places

--- part1/section3/main.zig
+++ part1/section3/main.zig
@@ -48,9 +48,9 @@ pub fn main() !void {
             const reg = (second_byte & mov_reg_mask) >> 3;
             const r_m = (second_byte & mov_r_m_mask);
 
+            var buf: [64]u8 = undefined;
             switch (mod) {
                 0b00 => {
-                    var buf: [64]u8 = undefined;
                     const direct_address: ?[]const u8 = if (r_m == 0b110) try readNext2BytesToNumString(u16, file_reader, &buf) else null;
                     if (d == 0) {
                         _ = try output_file.write(try r_m_mod0_lookup(r_m, direct_address));
@@ -63,7 +63,7 @@ pub fn main() !void {
                     }
                 },
                 0b01 => {
-                    const displacement = try file_reader.readByte();
+                    const displacement = try readNextByteToNumString(u8, file_reader, &buf);
                     if (d == 0) {
                         _ = try output_file.write(try r_m_mod1_lookup(r_m, displacement));
                         _ = try output_file.write(", ");
@@ -75,9 +75,7 @@ pub fn main() !void {
                     }
                 },
                 0b10 => {
-                    const disp_lo = try file_reader.readByte();
-                    const disp_hi = try file_reader.readByte();
-                    const displacement: u16 = (@as(u16, @intCast(disp_hi)) << 8) | @as(u16, @intCast(disp_lo));
+                    const displacement = try readNext2BytesToNumString(u16, file_reader, &buf);
                     if (d == 0) {
                         _ = try output_file.write(try r_m_mod2_lookup(r_m, displacement));
                         _ = try output_file.write(", ");
@@ -177,32 +175,32 @@ fn r_m_mod0_lookup(r_m: u8, direct_address_string: ?[]const u8) ![]const u8 {
     };
 }
 
-fn r_m_mod1_lookup(r_m: u8, displacement: u8) ![]const u8 {
+fn r_m_mod1_lookup(r_m: u8, displacement: []const u8) ![]const u8 {
     var buf: [64]u8 = undefined;
     return switch (r_m) {
-        0b000 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.SI.toString(), displacement }),
-        0b001 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.DI.toString(), displacement }),
-        0b010 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.SI.toString(), displacement }),
-        0b011 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.DI.toString(), displacement }),
-        0b100 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.SI.toString(), displacement }),
-        0b101 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.DI.toString(), displacement }),
-        0b110 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BP.toString(), displacement }),
-        0b111 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BX.toString(), displacement }),
+        0b000 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BX.toString(), Register.SI.toString(), displacement }),
+        0b001 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BX.toString(), Register.DI.toString(), displacement }),
+        0b010 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BP.toString(), Register.SI.toString(), displacement }),
+        0b011 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BP.toString(), Register.DI.toString(), displacement }),
+        0b100 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.SI.toString(), displacement }),
+        0b101 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.DI.toString(), displacement }),
+        0b110 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.BP.toString(), displacement }),
+        0b111 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.BX.toString(), displacement }),
         else => unreachable,
     };
 }
 
-fn r_m_mod2_lookup(r_m: u8, displacement: u16) ![]const u8 {
+fn r_m_mod2_lookup(r_m: u8, displacement: []const u8) ![]const u8 {
     var buf: [64]u8 = undefined;
     return switch (r_m) {
-        0b000 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.SI.toString(), displacement }),
-        0b001 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BX.toString(), Register.DI.toString(), displacement }),
-        0b010 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.SI.toString(), displacement }),
-        0b011 => std.fmt.bufPrint(&buf, "[{s} + {s} + {}]", .{ Register.BP.toString(), Register.DI.toString(), displacement }),
-        0b100 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.SI.toString(), displacement }),
-        0b101 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.DI.toString(), displacement }),
-        0b110 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BP.toString(), displacement }),
-        0b111 => std.fmt.bufPrint(&buf, "[{s} + {}]", .{ Register.BX.toString(), displacement }),
+        0b000 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BX.toString(), Register.SI.toString(), displacement }),
+        0b001 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BX.toString(), Register.DI.toString(), displacement }),
+        0b010 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BP.toString(), Register.SI.toString(), displacement }),
+        0b011 => std.fmt.bufPrint(&buf, "[{s} + {s} + {s}]", .{ Register.BP.toString(), Register.DI.toString(), displacement }),
+        0b100 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.SI.toString(), displacement }),
+        0b101 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.DI.toString(), displacement }),
+        0b110 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.BP.toString(), displacement }),
+        0b111 => std.fmt.bufPrint(&buf, "[{s} + {s}]", .{ Register.BX.toString(), displacement }),
         else => unreachable,
     };
 }
