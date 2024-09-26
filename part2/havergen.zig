b4d3274cef83e8df12574d8777c922e8f6cc3092
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-26 11:16:08 +0100

    parse args and generate random floats [0,1]

--- part2/havergen.zig
+++ part2/havergen.zig
@@ -14,11 +14,39 @@
 // }
 //
 //
+// Usage: havergen seed pair_count
+// Output: data_{seed}_{pair_count}.json
+//
+//
 
 const std = @import("std");
 const haversine = @import("haversine.zig").haversine;
 
 pub fn main() void {
-    const result = haversine(1, 2, 3, 4, 5);
-    std.debug.print("It's working {}\n", .{result});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const args = std.process.argsAlloc(allocator) catch fatal(null);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len != 3) fatal("USAGE: must pass 2 numeric arguments\n```\nhavergen seed num_pairs\n```\n");
+
+    const seed = std.fmt.parseInt(u64, args[1], 10) catch fatal("USAGE: seed argument must be non-negative integer");
+    const pair_count = std.fmt.parseInt(u32, args[2], 10) catch fatal("USAGE: pair_count argument must be non-negative integer");
+
+    var rng = std.rand.DefaultPrng.init(seed);
+    const rand = rng.random();
+    for (0..pair_count) |_| {
+        std.debug.print("{}\n", .{rand.float(f64)});
+    }
+}
+
+fn fatal(message: ?[]const u8) noreturn {
+    if (message) |m| {
+        const stderr = std.io.getStdErr();
+        _ = stderr.write(m) catch {};
+        _ = stderr.write("\n") catch {};
+    }
+    std.process.exit(1);
 }
