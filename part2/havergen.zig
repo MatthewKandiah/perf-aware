a3a68fa7c02d5a39856362e4655a5d6be6487ccb
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-26 10:51:47 +0100

    fleshed out explanatory comment

--- part2/havergen.zig
+++ part2/havergen.zig
@@ -3,13 +3,22 @@
 // Generate random json input for exercises.
 // Must take argument for number of pairs to generate.
 // Casey recommends adding some clustering in the data generation, so the randomly generated points don't have a predictable sum / average.
+// I also like his suggestion to have the generator print out the expected sum and dumping the expected values to a binary data file for quick debugging and checking.
+//
+//
+// Json schema:
+// {
+//   "pairs": [
+//     "x0": number, "y0": number, "x1": number, "y1": number
+//   ]
+// }
 //
 //
 
 const std = @import("std");
-const haversine = @import("haversine.zig");
+const haversine = @import("haversine.zig").haversine;
 
 pub fn main() void {
-    const result = haversine.haversine(1, 2, 3, 4, 5);
+    const result = haversine(1, 2, 3, 4, 5);
     std.debug.print("It's working {}\n", .{result});
 }
